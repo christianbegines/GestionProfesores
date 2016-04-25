83879638f65b3359a4c65cdde58a6686f9197207
christianbegines <christianbeginescarrasco@gmail.com>
2016-04-25 09:50:12 +0200

    primera subida

---
+++ src/gestionprofesores/Profesor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionprofesores;
+
+/**
+ *nif, su nombre, la departamento y su edad.
+ *Incluye un método toString() que concatene todos los atributos y lo muestre por pantalla. 
+ * @author daw1
+ */
+public class Profesor {
+    private
+    private String dni;
+    private String departamento;
+    int edad;
+    
+}
