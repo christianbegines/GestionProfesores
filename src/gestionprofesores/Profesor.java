c6f56a50b980595bf22d080de330738263ec8826
christianbegines <christianbeginescarrasco@gmail.com>
2016-04-25 10:10:09 +0200

    crear construcotr, clase profesor

--- src/gestionprofesores/Profesor.java
+++ src/gestionprofesores/Profesor.java
@@ -5,15 +5,67 @@
  */
 package gestionprofesores;
 
+import java.io.Serializable;
+
 /**
  *nif, su nombre, la departamento y su edad.
  *Incluye un método toString() que concatene todos los atributos y lo muestre por pantalla. 
  * @author daw1
  */
-public class Profesor {
-    private
+public class Profesor implements Serializable{
+    private String nombre;
     private String dni;
     private String departamento;
-    int edad;
+    private int edad;
+
+    public Profesor(String nombre, String dni, String departamento, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.departamento = departamento;
+        this.edad = edad;
+    }
     
+    public Profesor(){
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: "+ nombre +".\n"+
+               "Dni:"+dni+".\n"+
+               "Departamento:"+".\n"+
+               "Edad:"+ edad;    
+    }
 }
